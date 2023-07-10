7fc8e1f2ce2aa665a030b9ef7ddb42d2fbf33a56
Salma Mouayad <Salma.Mouayad@emsi-edu.ma>
2023-07-11 00:54:36 +0100

    fourth commit

--- backend/src/main/java/com/leave/backend/BackendApplication.java
+++ backend/src/main/java/com/leave/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package com.leave.pfa;
+package com.leave.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PfaApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PfaApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
--- backend/src/test/java/com/leave/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/leave/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.leave.pfa;
+package com.leave.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PfaApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
