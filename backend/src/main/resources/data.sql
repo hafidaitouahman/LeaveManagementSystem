2690a568ff6a80ce2a3da66838fc8225239a1f04
hafidaitouahman <hafid.aitouahman>
2024-09-11 12:17:35 +0100

    change application.properties

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,23 @@
+INSERT IGNORE INTO `leavems`.`roles`(`id`,`name`) VALUES(1,'ROLE_USER');
+INSERT IGNORE INTO `leavems`.`roles`(`id`,`name`) VALUES(2,'ROLE_RH');
+
+
+INSERT IGNORE INTO `leavems`.`departement`(`id`,`name`) VALUES (1, 'Production');
+INSERT IGNORE INTO `leavems`.`departement`(`id`,`name`) VALUES (2, 'Financial accounting');
+INSERT IGNORE INTO `leavems`.`departement`(`id`,`name`) VALUES (3, 'IT');
+INSERT IGNORE INTO `leavems`.`departement`(`id`,`name`) VALUES (4, 'Human Resources');
+INSERT IGNORE INTO `leavems`.`departement`(`id`,`name`) VALUES (5, 'Marketing');
+INSERT IGNORE INTO `leavems`.`departement`(`id`,`name`) VALUES (6, 'Business development');
+INSERT IGNORE INTO `leavems`.`departement`(`id`,`name`) VALUES (7, 'Customer service');
+INSERT IGNORE INTO `leavems`.`departement`(`id`,`name`) VALUES (8, 'Research and development');
+INSERT IGNORE INTO `leavems`.`departement`(`id`,`name`) VALUES (9, 'Business administration');
+INSERT IGNORE INTO `leavems`.`departement`(`id`,`name`) VALUES (10, 'Sales');
+INSERT IGNORE INTO `leavems`.`departement`(`id`,`name`) VALUES (11, 'Purchasing');
+INSERT IGNORE INTO `leavems`.`departement`(`id`,`name`) VALUES (12, 'Store');
+INSERT IGNORE INTO `leavems`.`departement`(`id`,`name`) VALUES (13, 'Quality management');
+
+INSERT IGNORE INTO `leavems`.`site`(`id`,`name`) VALUES(1,'Site 1');
+INSERT IGNORE INTO `leavems`.`site`(`id`,`name`) VALUES(2,'Site 2');
+
+INSERT IGNORE INTO `leavems`.`teams`(`id`,`name`) VALUES (1,'Equipe 1');
+INSERT IGNORE INTO `leavems`.`teams`(`id`,`name`) VALUES (2,'Equipe 2');
